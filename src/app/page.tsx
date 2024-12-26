9cd110876e46156fdaf668a8fe459a2910cfb3a4
atina <atinachang@gmail.com>
2024-12-26 14:42:06 -0500

    set up fonts, text cl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="flex flex-col justify-center items-center w-1/2 text-center m-auto">
+        <h1 className="text-4xl font-bold">Startup Landing Page</h1>
+        <p className="my-8">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos laudantium, error fugiat doloremque excepturi
+          officia!
+        </p>
+      </div>
     </>
   );
 }
