6fee8149d37eef49186693dbf1ae00ce2b5deb9f
atina <atinachang@gmail.com>
2024-12-27 17:56:48 -0500

    adjust z-index

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Success = () => {
   return (
-    <div className="my-16">
+    <div className="my-16 z-10 h-screen relative">
       <h2 className="text-4xl text-center">You have successfully signed up for our mailing list!</h2>
     </div>
   );
