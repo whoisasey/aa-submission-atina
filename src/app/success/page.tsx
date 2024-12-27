e6af06e2c465f8c25703f1c104619833c50f602a
atina <56001902+whoisasey@users.noreply.github.com>
2024-12-27 18:04:02 -0500

    Fix success page (#14)
    * adjust z-index
    
    * add countdown to redirect user

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { useRouter } from "next/navigation";
 
 const Success = () => {
+  const [countdown, setCountdown] = useState(8);
+  const router = useRouter();
+
+  // adds a countdown to redirect the client from the success apge
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown((prev) => prev - 1);
+    }, 1000);
+
+    const timeout = setTimeout(() => {
+      router.push("/");
+    }, countdown * 1000);
+
+    return () => {
+      clearInterval(interval);
+      clearTimeout(timeout);
+    };
+  }, [router, countdown]);
+
   return (
-    <div className="my-16">
+    <div className="my-16 z-10 h-screen relative">
       <h2 className="text-4xl text-center">You have successfully signed up for our mailing list!</h2>
+      <p className="text-center mt-8">You will now be redirected... {countdown}</p>
     </div>
   );
 };
