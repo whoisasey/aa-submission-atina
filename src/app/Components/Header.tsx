f0402237adb30e28c78fa1dc59eda0a0cdd51255
atina <atinachang@gmail.com>
2024-12-27 13:07:16 -0500

    convert to sass

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 import logo from "@/assets/logo.svg";
 
 const Header = () => {
+  // TODO: set timeout so that redirects to home after 5 seconds, add countdown?
   return (
-    <header className="absolute top-0 left-8 sm:left-20 lg:left-96 z-0">
-      <Image src={logo} alt="Agency Analytics logo" />
+    // <header className="absolute top-0 left-8 sm:left-20 lg:left-96 z-0">
+    <header>
+      <Link href={"/"}>
+        <Image src={logo} alt="Agency Analytics logo" />
+      </Link>
     </header>
   );
 };
