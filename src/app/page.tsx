b374d3a57fef94099e14b9e981991b0e699c7f9c
atina <atinachang@gmail.com>
2025-01-02 14:57:18 -0500

    extract helpers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,84 +1,17 @@
 "use client";
 
+import { BubbleImage, FeatureCard, bubbles, features } from "./helpers";
 import { FormEvent, useState } from "react";
 
 import Image from "next/image";
 import background2 from "@/assets/feature-background.svg";
 import browser from "@/assets/browser.svg";
-import bubble1 from "@/assets/bubbles/bubble-1.svg";
-import bubble2 from "@/assets/bubbles/bubble-2.svg";
-import bubble3 from "@/assets/bubbles/bubble-3.svg";
-import bubble4 from "@/assets/bubbles/bubble-4.svg";
-import feature1 from "@/assets/features/feature-1.svg";
-import feature2 from "@/assets/features/feature-2.svg";
-import feature3 from "@/assets/features/feature-3.svg";
-import feature4 from "@/assets/features/feature-4.svg";
 import { useRouter } from "next/navigation";
-
-const BubbleImage = ({ src, className }: { src: string; className: string }) => (
-  <div className={`absolute ${className}`}>
-    <Image src={src} alt="bubble" />
-  </div>
-);
-
-const FeatureCard = ({
-  src,
-  title,
-  description,
-  className,
-}: {
-  src: string;
-  title: string;
-  description: string;
-  className?: string;
-}) => (
-  <div className={`flex flex-col justify-center items-center mx-auto my-8 text-center`}>
-    <div className={`${className} flex flex-col justify-center items-center mx-auto`}>
-      <div className="bg-white rounded-full shadow-slate-200/50 shadow-xl w-fit">
-        <Image src={src} alt={title} />
-      </div>
-    </div>
-    <h4 className="font-bold text-xl my-3">{title}</h4>
-    <p>{description}</p>
-  </div>
-);
-
-const lorumDesc = "Suspendisse nec turpis quis egestas sit augue. Pellentesque habitant morbi tristique senectus.";
 
 const Home = () => {
   const [email, setEmail] = useState("");
 
   const router = useRouter();
-
-  const bubbles = [
-    { src: bubble3, className: "left-20 sm:-left-8 -top-4 sm:top-20 z-50" },
-    { src: bubble2, className: "-top-8 -right-20 sm:-right-32 w-5/12 sm:w-auto z-20" },
-    { src: bubble1, className: "-left-16 sm:-left-32 -bottom-16 sm:-bottom-24 w-1/2 sm:w-auto z-20" },
-    { src: bubble4, className: "-bottom-4 sm:bottom-0 -right-8 sm:-right-16 w-1/4 sm:w-auto z-50" },
-  ];
-
-  const features = [
-    {
-      src: feature1,
-      title: "Discover",
-      description: lorumDesc,
-    },
-    {
-      src: feature2,
-      title: "Discover",
-      description: lorumDesc,
-    },
-    {
-      src: feature3,
-      title: "Discover",
-      description: lorumDesc,
-    },
-    {
-      src: feature4,
-      title: "Discover",
-      description: lorumDesc,
-    },
-  ];
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
@@ -139,9 +72,6 @@ const Home = () => {
         <div className="relative w-full">
           <div className="relative z-30 shadow-xl shadow-slate-300/50">
             <Image src={browser} alt="browser" />
-          </div>
-          <div className="absolute left-20 sm:-left-8 -top-4 sm:top-20 z-50">
-            <Image src={bubble3} alt="bubble" />
           </div>
           {bubbles.map((bubble, idx) => (
             <BubbleImage key={idx} src={bubble.src} className={bubble.className} />
